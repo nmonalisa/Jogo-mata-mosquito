6888935ad12fd6b061cf8331a5ebe5baf96f78ba
N. Monalisa <nathaliamonalisa@gmail.com>
2020-02-23 14:20:32 -0300

    implementando a lógica de funcionamento do botão Iniciar

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 <p> Objetivo do jogo: eliminar todos os mosquitos e impedir uma nova epidemia do século XXI chamada 'devmortus'.<br/>
 Essa doença é altamente contagiosa e perigosa, pois ataca a personalidade de programador@s fazendo com que el@s deixem de se interessar por camisetas, seriados e filmes nerds como Star Wars! :laughing: </p>
 <p>A imagem do cenário foi cedida pelo curso </p>
-<p>**Em construção!** </p>
+<p>**Em construção!**</p>
--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 //Função para ajustar tamanho da tela quando o body do documento for redimensionado
-function resizeScreen(){
-window.resizeTo(window.innerWidth, window.innerHeight)
-}
-
+resizeScreen = () => window.resizeTo(window.innerWidth, window.innerHeight);
 //Variáveis globais
-let enemy = document.getElementById('enemy') 
+let enemy = document.getElementById('enemy')
 let posicao = Array()
-
+//Ajustar níveis de dificuldade
+let level
+function selectLevel(element){
+	switch (element.value) {    
+		case '--Selecione um nível--':
+        level = "";
+        case 'easy':
+		level = 3000;
+        case 'normal':
+        level = 2000;
+        case 'hard':
+        level = 1000;
+        }
+}
 //Função para criar posições aleatórias
 function createPosition(){
-posicao[0] = Math.floor(Math.random()*(window.innerWidth -250)) + 'px';
-posicao[1] = Math.floor(Math.random()*(window.innerHeight-250))+ 'px';
-console.log(posicao)
+posicao[0] = Math.floor(Math.random()*(window.innerWidth -150)) + 'px'
+posicao[1] = Math.floor(Math.random()*(window.innerHeight-150)) + 'px';
 return posicao
 }
 //Função para criar mosquitos: posição, tamanho e sentido do mosquito são aleatórios
@@ -29,12 +38,25 @@ enemy.setAttribute('class', size_enemy[index_size] + " " + rotation[index_rot]);
 //atribuindo posição aleatória a cada mosquito criado
 enemy.style.left = vetor_posicao[0];
 enemy.style.top = vetor_posicao[1];
-
 return
 }
-//Iniciar game
-function startGame(){
-showEnemy(createPosition())
+//criando temporizador
+function myTimer(time){
+    setInterval(function(){
+    if (time >= 0){
+    let timer = document.getElementById('timer')
+    timer.innerHTML = `Tempo: ${time}"`
+    time -= 1
+    } else {
+        //forcar parada do jogo
+    }}
+    ,1000)
+    }
+//Iniciar jogo
+startGame = () => showEnemy(createPosition());
+//Iniciar cronometro
+startTimer = () => myTimer(90);
+startStatus =  () => {
+    document.getElementById('status').style.opacity = 0.6;
+    document.getElementById('enemy').style.opacity = 1.0;
 }
-
-
